8818bd226d275e4e20f64271e49fdda8ab3acc96
bmarzolf <bmarzolf@53153c1e-2216-0410-80fa-f1be67669deb>
2006-08-10 22:18:18 +0000

    Had to do a reset_column_information so that update of new column would work.

--- db/migrate/009_add_administrator_email.rb
+++ db/migrate/009_add_administrator_email.rb
@@ -1,8 +1,9 @@
 class AddAdministratorEmail < ActiveRecord::Migration
   def self.up
     add_column :site_config, :administrator_email, :string, :limit => 100
-
-    SiteConfig.update 1, :administrator_email => 'webmaster@your.site.com'
+    
+    SiteConfig.reset_column_information
+    SiteConfig.update 1, :administrator_email => "webmaster@your.site.com"
   end
 
   def self.down
