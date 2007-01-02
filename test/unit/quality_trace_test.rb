fa67651e0410a834f9e01197776388923bf06415
bmarzolf <bmarzolf@53153c1e-2216-0410-80fa-f1be67669deb>
2007-01-02 21:21:22 +0000

    Test for nullification of associated samples upon destroy.

--- test/unit/quality_trace_test.rb
+++ test/unit/quality_trace_test.rb
@@ -3,8 +3,13 @@ require File.dirname(__FILE__) + '/../test_helper'
 class QualityTraceTest < Test::Unit::TestCase
   fixtures :quality_traces
 
-  # Replace this with your real tests.
-  def test_truth
-    assert true
+  def test_destroy_with_samples_associated
+    QualityTrace.find(6).destroy
+    
+    assert_raise(ActiveRecord::RecordNotFound) {
+      QualityTrace.find(6)
+    }
+    
+    assert_nil Sample.find(1).starting_quality_trace_id
   end
 end
