74fbdf18e7365e38fc1c8f594b4da7f3a4c3f673
bmarzolf <bmarzolf@53153c1e-2216-0410-80fa-f1be67669deb>
2008-05-15 16:20:59 +0000

    Sample export to Excel.

--- app/models/sample.rb
+++ app/models/sample.rb
@@ -1,8 +1,13 @@
 class Sample < ActiveRecord::Base
+  require 'spreadsheet/excel'
+  require 'parseexcel'
+  include Spreadsheet
+  
   has_one :hybridization, :dependent => :destroy
 
   belongs_to :chip_type
   belongs_to :project
+  belongs_to :organism
   belongs_to :starting_quality_trace, :class_name => "QualityTrace", :foreign_key => "starting_quality_trace_id"
   belongs_to :amplified_quality_trace, :class_name => "QualityTrace", :foreign_key => "amplified_quality_trace_id"
   belongs_to :fragmented_quality_trace, :class_name => "QualityTrace", :foreign_key => "fragmented_quality_trace_id"
@@ -45,5 +50,143 @@ class Sample < ActiveRecord::Base
        sample_name[/\\/] != nil
       errors.add("Sample name must contain only letters, numbers, underscores and dashes or it")
     end
+  end
+  
+  def self.to_excel
+    ###########################################
+    # set up spreadsheet
+    ###########################################
+    
+    puts "VERSION: " + Excel::VERSION
+    
+    workbook_name = "#{RAILS_ROOT}/tmp/excel/samples_" + Date.today.to_s +
+      ".xls"
+    
+    workbook = Excel.new(workbook_name)
+    # doing each side individually, since :border => 1 is giving an error
+    bordered = Format.new( :bottom => 1,
+                           :top => 1,
+                           :left => 1,
+                           :right => 1 )
+    bordered_bold = Format.new( :bottom => 1,
+                                :top => 1,
+                                :left => 1,
+                                :right => 1,
+                                :bold => true )
+    workbook.add_format(bordered)
+    workbook.add_format(bordered_bold)
+    
+    ###########################################
+    # tab for non-naming schemed samples
+    ###########################################
+    
+    unschemed = workbook.add_worksheet("no naming scheme")
+
+    
+    unschemed.write_row 0, 0, [ "Sample ID",
+                                "Submission Date",
+                                "Short Sample Name",
+                                "Sample Name",
+                                "Sample Group Name",
+                                "Chip Type",
+                                "Organism",
+                                "SBEAMS User",
+                                "Project",
+                                "Status",
+                              ], bordered
+
+    samples = Sample.find( :all, :conditions => {:naming_scheme_id => nil},
+      :include => [:project, :chip_type, :organism], :order => "samples.id ASC" )
+    
+    current_row = 0
+    for sample in samples
+      unschemed.write_row current_row+=1, 0, [ sample.id,
+                                             sample.submission_date.to_s,
+                                             sample.short_sample_name,
+                                             sample.sample_name,
+                                             sample.sample_group_name,
+                                             sample.chip_type.name,
+                                             sample.organism.name,
+                                             sample.sbeams_user,
+                                             sample.project.name,
+                                             sample.status
+      ], bordered
+    end
+
+    ###########################################
+    # tabs for naming schemed samples
+    ###########################################
+    
+    schemes = NamingScheme.find(:all, :order => "name ASC")
+    schemes.each do |scheme|
+      worksheet = workbook.add_worksheet(scheme.name)
+
+      # stock headings
+      headings = [ "Sample ID",
+        "Submission Date",
+        "Short Sample Name",
+        "Sample Name",
+        "Sample Group Name",
+        "Chip Type",
+        "Organism",
+        "SBEAMS User",
+        "Project",
+        "Status",
+      ]
+
+      # headings for naming elements
+      scheme.naming_elements.each do |e|
+        headings << e.name
+      end
+      
+      worksheet.write_row 0, 0, headings, bordered
+
+      samples = Sample.find( :all, 
+        :conditions => {:naming_scheme_id => scheme.id},
+        :include => [:project, :chip_type, :organism],
+        :order => "samples.id ASC" )
+
+      current_row = 0
+      for sample in samples
+        column_values = [ sample.id,
+          sample.submission_date.to_s,
+          sample.short_sample_name,
+          sample.sample_name,
+          sample.sample_group_name,
+          sample.chip_type.name,
+          sample.organism.name,
+          sample.sbeams_user,
+          sample.project.name,
+          sample.status
+        ]
+        # values for naming elements
+        scheme.naming_elements.each do |e|
+          value = ""
+          if(e.free_text == true)
+            sample_text = SampleText.find(:first, 
+              :conditions => {:sample_id => sample.id,
+              :naming_element_id => e.id})
+            if(sample_text != nil)
+              value = sample_text.text
+            end
+          else
+            sample_term = SampleTerm.find(:first,
+              :include => :naming_term,
+              :conditions => ["sample_id = ? AND naming_element_id = ?",
+                sample.id, e.id] )
+            if(sample_term != nil)
+              value = sample_term.naming_term.term
+            end
+          end
+          column_values << value
+        end
+        
+        worksheet.write_row current_row+=1, 0, column_values, bordered
+      end
+    end    
+    
+    workbook.close
+    
+    return workbook_name
   end
 end
--- test/unit/sample_test.rb
+++ test/unit/sample_test.rb
@@ -1,10 +1,147 @@
 require File.dirname(__FILE__) + '/../test_helper'
+require 'parseexcel'
 
 class SampleTest < Test::Unit::TestCase
-  fixtures :samples
+  fixtures :all
 
-  # Replace this with your real tests.
-  def test_truth
-    assert true
+  def test_to_excel
+    excel_file = Sample.to_excel
+    
+    workbook = Spreadsheet::ParseExcel.parse(excel_file)
+    
+    #################################
+    # check unschemed samples
+    #################################
+    
+    worksheet = workbook.worksheet(0)
+
+    # heading
+    assert_row_equal([
+      "Sample ID",
+      "Submission Date",
+      "Short Sample Name",
+      "Sample Name",
+      "Sample Group Name",
+      "Chip Type",
+      "Organism",
+      "SBEAMS User",
+      "Project",
+      "Status"
+    ], worksheet.row(0))
+    
+    # samples
+    assert_row_equal([
+      "1.0",
+      "2006-02-10",
+      "yng",
+      "Young",
+      "Young",
+      "Alligator 670 2.0",
+      "Mouse",
+      "bob",
+      "MouseGroup",
+      "submitted"
+    ], worksheet.row(1))
+    
+    assert_row_equal([
+      "2.0",
+      "2006-02-10",
+      "old",
+      "Old",
+      "Old",
+      "Alligator 670 2.0",
+      "Mouse",
+      "bob",
+      "MouseGroup",
+      "hybridized"
+    ], worksheet.row(2))
+    
+    assert_row_equal([
+      "3.0",
+      "2006-02-10",
+      "vold",
+      "Very Old",
+      "Old",
+      "Alligator 670 2.0",
+      "Mouse",
+      "bob",
+      "MouseGroup",
+      "submitted"
+    ], worksheet.row(3))
+    
+    assert_row_equal([
+      "4.0",
+      "2006-02-10",
+      "vvold",
+      "Very Very Old",
+      "Old",
+      "Alligator 670 2.0",
+      "Mouse",
+      "bob",
+      "MouseGroup",
+      "hybridized"
+    ], worksheet.row(4))
+    
+    assert_row_equal([
+      "5.0",
+      "2006-09-10",
+      "bb",
+      "Bob B",
+      "Bob",
+      "Alligator 670 2.0",
+      "Mouse",
+      "bob",
+      "Bob's Stuff",
+      "submitted"
+    ], worksheet.row(5))
+  
+    #################################
+    # check schemed samples
+    #################################
+    
+    worksheet = workbook.worksheet(2)
+    assert_row_equal([
+      "Sample ID",
+      "Submission Date",
+      "Short Sample Name",
+      "Sample Name",
+      "Sample Group Name",
+      "Chip Type",
+      "Organism",
+      "SBEAMS User",
+      "Project",
+      "Status",
+      "Strain",
+      "Perturbation",
+      "Perturbation Time",
+      "Replicate",
+      "Subject Number",
+    ], worksheet.row(0))
+    
+    assert_row_equal([
+      "6.0",
+      "2007-05-31",
+      "a1",
+      "wt_HT_024_B_32234",
+      "wt_HT_024",
+      "Alligator 670 2.0",
+      "Mouse",
+      "bob",
+      "Bob's Stuff",
+      "submitted",
+      "wild-type",
+      "heat",
+      "024",
+      "B",
+      "32234"      
+    ], worksheet.row(1))
+  end
+  
+  def assert_row_equal(expected, row)
+    column = 0
+    expected.each do |cell|
+      assert_equal cell, row.at(column).to_s
+      column += 1
+    end
   end
 end
