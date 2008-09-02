490d26c2ebaf15df1f2e87238020b81891d74de9
bmarzolf <bmarzolf@53153c1e-2216-0410-80fa-f1be67669deb>
2008-09-02 22:26:19 +0000

    Added debugging output for Bioanalyzer import.

--- app/models/bioanalyzer_run.rb
+++ app/models/bioanalyzer_run.rb
@@ -1,3 +1,5 @@
+VERBOSE = 0
+
 class BioanalyzerRun < ActiveRecord::Base
   has_many :quality_traces, :dependent => :destroy
 
@@ -11,9 +13,9 @@ class BioanalyzerRun < ActiveRecord::Base
     bioanalyzer_pickup_location = SiteConfig.bioanalyzer_pickup
     
     # get a list of the XML and JPG files
-    xml_file_pattern = File.join(bioanalyzer_pickup_location,"/","**", "*.xml")
+    xml_file_pattern = File.join(bioanalyzer_pickup_location,"/*.xml")
     xml_files = Dir.glob(xml_file_pattern)
-    jpg_file_pattern = File.join(bioanalyzer_pickup_location,"/","**", "*.jpg")
+    jpg_file_pattern = File.join(bioanalyzer_pickup_location,"/*.jpg")
     jpg_files = Dir.glob(jpg_file_pattern)
 
     # find all the bioanalyzer runs that are already in the database
@@ -24,6 +26,8 @@ class BioanalyzerRun < ActiveRecord::Base
     end
     
     for xml_file in xml_files
+      puts "Examining XML file: #{xml_file}" if VERBOSE > 0
+      
       # name of run is filename minus extension
       run_name = xml_file.scan(/.*\/(.*?)\.xml/i).to_s
 
@@ -37,13 +41,15 @@ class BioanalyzerRun < ActiveRecord::Base
         
         # find the chip-wide lab group
         chip_lab_name = doc.elements["Chipset/Chips/Chip/Files/File/FileInformation/Comment"].text
+        puts "Lab name: #{chip_lab_name}" if VERBOSE > 0
 
-		    if( chip_lab_name != nil )
-	        # get rid of trailing whitespace
-	        chip_lab_name = chip_lab_name.scan(/(.*)\n*/)[0]
-	        chip_lab_group = LabGroup.find(:first, :conditions => [ "name = ?", chip_lab_name])
-    	  end
-    
+        if( chip_lab_name != nil )
+          # get rid of trailing whitespace
+          chip_lab_name = chip_lab_name.scan(/(.*)\n*/)[0]
+          chip_lab_group = LabGroup.find(:first, :conditions => [ "name = ?", chip_lab_name])
+        end
+        puts "SLIMarray lab group: #{chip_lab_group}" if VERBOSE > 0
+        
         # create an array to hold traces
         traces = Array.new
         
@@ -59,11 +65,11 @@ class BioanalyzerRun < ActiveRecord::Base
 
             # figure out sample name and type
             full_name = node.elements["Name"].text
-            if( full_name.downcase.match(/(.*)_(total|crna|frag|fragmented)/) != nil )
+            if( full_name.downcase.match(/(.*)_(total|crna|frag|fragmented).*/) != nil )
               elements = full_name.split(/_/)
               type = elements.pop
               name = elements.join("_")
-            elsif( full_name.downcase.match(/(.*)\ (total|crna|frag|fragmented)/) != nil )
+            elsif( full_name.downcase.match(/(.*)\ (total|crna|frag|fragmented).*/) != nil )
               elements = full_name.split(/ /)
               type = elements.pop
               name = elements.join(" ")
@@ -101,7 +107,7 @@ class BioanalyzerRun < ActiveRecord::Base
               ribosomal_ratio =
                 node.elements["DAResultStructures/DARFragment/Channel/rRNARatio"].text
             end
-  
+
             # ensure that sample is tagged with a type, and that image exists
             if( type != nil && lab_group != nil && jpg_files.include?(current_jpg_file) )
               # create new image name, and drop spaces to make the URL easier
@@ -131,14 +137,14 @@ class BioanalyzerRun < ActiveRecord::Base
               end
 
               trace = QualityTrace.new(:image_path => image_path,
-                                       :quality_rating => quality_rating,
-                                       :name => name,
-                                       :number => number,
-                                       :sample_type => type,
-                                       :concentration => concentration,
-                                       :ribosomal_ratio => ribosomal_ratio,
-                                       :lab_group_id => lab_group.id
-                                       )
+                :quality_rating => quality_rating,
+                :name => name,
+                :number => number,
+                :sample_type => type,
+                :concentration => concentration,
+                :ribosomal_ratio => ribosomal_ratio,
+                :lab_group_id => lab_group.id
+              )
               traces << trace
             end
           end
@@ -151,8 +157,8 @@ class BioanalyzerRun < ActiveRecord::Base
           date = time_date.scan(/(\d{4}\-\d{2}\-\d{2}).*/).to_s
                   
           run = BioanalyzerRun.new(:name => run_name,
-                                   :date => date
-                                   )
+            :date => date
+          )
           
           # save the traces if the run itself saves
           if( run.save )
@@ -170,8 +176,8 @@ class BioanalyzerRun < ActiveRecord::Base
     quality_traces = QualityTrace.find(:all, :conditions => ["bioanalyzer_run_id = ?", id])
     
     return "Destroying this Bioanalyzer Run will also destroy:\n" + 
-           quality_traces.size.to_s + " trace(s)\n" +
-           "Are you sure you want to destroy it?"
+      quality_traces.size.to_s + " trace(s)\n" +
+      "Are you sure you want to destroy it?"
   end
 
 end
