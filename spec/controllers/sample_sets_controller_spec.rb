0acf0935e17a54791aca82e699c29bde973040e2
bmarzolf <bmarzolf@53153c1e-2216-0410-80fa-f1be67669deb>
2008-10-07 21:19:42 +0000

    Spec for sample_sets/create finished and working.

--- spec/controllers/sample_sets_controller_spec.rb
+++ spec/controllers/sample_sets_controller_spec.rb
@@ -190,53 +190,110 @@ describe SampleSetsController do
     end
   end
   
-#  describe "handling POST /sample_sets" do
-#
-#    before(:each) do
-#      @sample_set = mock_model(SampleSet, :to_param => "1")
-#      SampleSet.stub!(:new).and_return(@sample_set)
-#    end
-#    
-#    describe "with successful save" do
-#  
-#      def do_post
-#        @sample_set.should_receive(:save).and_return(true)
-#        post :create,
-#          :sample_set => {"submission_date(2i)"=>"10", "naming_scheme_id"=>"",
-#                          "sample_prep_kit_id"=>"1", "number_of_samples"=>"2",
-#                          "submission_date(3i)"=>"6", "alignment_end_position"=>"18",
-#                          "desired_read_length"=>"18", "reference_genome_id"=>"2",
-#                          "alignment_start_position"=>"1", "lab_group_id"=>"1",
-#                          "budget_number"=>"1234", "insert_size"=>"150",
-#                          "submission_date(1i)"=>"2008"},
-#           :sample => {}
-#      end
-#  
-#      it "should create a new sample_set" do
-#        SampleSet.should_receive(:new).with({}).and_return(@sample_set)
-#        do_post
-#      end
-#
-#      it "should redirect to the list of samples" do
-#        do_post
-#        response.should redirect_to('samples/list')
-#      end
-#      
-#    end
-#    
-#    describe "with failed save" do
-#
-#      def do_post
-#        @sample_set.should_receive(:save).and_return(false)
-#        post :create, :sample_set => {}
-#      end
-#  
-#      it "should re-render 'new' template at step 2" do
-#        do_post
-#        response.should render_template('new')
-#      end
-#      
-#    end
-#  end
+  describe "handling POST /sample_sets" do
+    before(:each) do
+      @sample_set = mock_model(SampleSet, :to_param => "1")
+      SampleSet.stub!(:new).and_return(@sample_set)
+      @sample = mock_model(Sample)
+      Sample.stub!(:new).and_return(@sample)
+      @sample_set.stub!(:samples=).and_return(true)
+    end
+   
+    describe "with a valid sample set" do
+      before(:each) do
+        @sample_set.stub!(:valid?).and_return(true)
+        @sample.stub!(:save).and_return(true)
+      end
+  
+      def do_post
+        post :create,
+          :sample_set => {"submission_date(2i)"=>"10", "naming_scheme_id"=>"",
+                          "sample_prep_kit_id"=>"1", "number_of_samples"=>"2",
+                          "submission_date(3i)"=>"6", "alignment_end_position"=>"18",
+                          "desired_read_length"=>"18", "reference_genome_id"=>"2",
+                          "alignment_start_position"=>"1", "lab_group_id"=>"1",
+                          "budget_number"=>"1234", "insert_size"=>"150",
+                          "submission_date(1i)"=>"2008"},
+           :sample => {"0"=>{"status"=>"", "reference_genome_id"=>"1",
+             "short_sample_name"=>"1121", "desired_read_length"=>"18",
+             "schemed_name"=>{"Protocol"=>"203", "Cell Type"=>"197", "Time"=>"184",
+             "Biological Replicate"=>"", "Antibody"=>"291", "Stimulus"=>"189",
+             "Exclude From Analysis"=>"204", "Technical Replicate"=>"202", "Date"=>""},
+             "submission_date"=>"2008-10-02", "budget_number"=>"1234", "insert_size"=>"150",
+             "sample_prep_kit_id"=>"1"} }
+      end
+  
+      it "should create a new sample_set" do
+        SampleSet.should_receive(:new).and_return(@sample_set)
+        do_post
+      end
+      
+      it "should find the new sample set valid" do
+        @sample_set.should_receive(:valid?).and_return(true)
+        do_post
+      end
+      
+      it "should save the sample" do
+        @sample.should_receive(:save).and_return(true)
+        do_post
+      end      
 
+      it "should redirect to the list of samples" do
+        do_post
+        response.should redirect_to('samples/list')
+      end
+    end
+    
+    describe "with an invalid sample set" do
+      before(:each) do
+        @sample_set.stub!(:valid?).and_return(false)
+        @naming_scheme = mock_model(NamingScheme)
+        @sample_set.stub!(:naming_scheme).and_return(@naming_scheme)
+        @naming_scheme.stub!(:ordered_naming_elements).and_return( [mock_model(NamingElement)] )
+      end
+
+      def do_post
+        post :create,
+          :sample_set => {"submission_date(2i)"=>"10", "naming_scheme_id"=>"",
+                          "sample_prep_kit_id"=>"1", "number_of_samples"=>"2",
+                          "submission_date(3i)"=>"6", "alignment_end_position"=>"18",
+                          "desired_read_length"=>"18", "reference_genome_id"=>"2",
+                          "alignment_start_position"=>"1", "lab_group_id"=>"1",
+                          "budget_number"=>"1234", "insert_size"=>"150",
+                          "submission_date(1i)"=>"2008"},
+           :sample => {"0"=>{"status"=>"", "reference_genome_id"=>"1",
+             "short_sample_name"=>"1121", "desired_read_length"=>"18",
+             "schemed_name"=>{"Protocol"=>"203", "Cell Type"=>"197", "Time"=>"184",
+             "Biological Replicate"=>"", "Antibody"=>"291", "Stimulus"=>"189",
+             "Exclude From Analysis"=>"204", "Technical Replicate"=>"202", "Date"=>""},
+             "submission_date"=>"2008-10-02", "budget_number"=>"1234", "insert_size"=>"150",
+             "sample_prep_kit_id"=>"1"} }
+      end
+
+      it "should find the new sample set invalid" do
+        @sample_set.should_receive(:valid?).and_return(false)
+        do_post
+      end
+
+      it "should get the naming scheme for the sample set" do
+        @sample_set.should_receive(:naming_scheme).and_return(@naming_scheme)
+        do_post
+      end
+      
+      it "should get the naming elements for the naming scheme" do
+        @naming_scheme.should_receive(:ordered_naming_elements).and_return( [mock_model(NamingElement)] )
+        do_post
+      end
+      
+      it "should set the step parameter to go to step 2" do
+        do_post
+        params[:step].should == "2"
+      end
+  
+      it "should re-render 'new' template at step 2" do
+        do_post
+        response.should render_template('new')
+      end
+    end
+  end
 end
