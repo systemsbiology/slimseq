846dcb49c322f418dd213dedd5146d48500e5360
bmarzolf <bmarzolf@53153c1e-2216-0410-80fa-f1be67669deb>
2008-10-08 17:33:33 +0000

    Test for setting lane attributes for a flow cell.

--- spec/models/flow_cell_spec.rb
+++ spec/models/flow_cell_spec.rb
@@ -2,8 +2,17 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe FlowCell do
   describe "setting lane attributes" do
-    before(:each) do
-      
+    it "should build lanes from a set of attributes" do
+      @flow_cell = FlowCell.new
+      @flow_cell_lanes = [FlowCellLane.new,FlowCellLane.new]
+      @flow_cell.should_receive(:flow_cell_lanes).twice.and_return(@flow_cell_lanes)
+      @flow_cell_lanes.should_receive(:build).exactly(2).times
+      @flow_cell.lane_attributes = [
+        {"starting_concentration"=>"234", "loaded_concentration"=>"987",
+         "sample_id"=>"7", "lane_number"=>"1"},
+        {"starting_concentration"=>"0987", "loaded_concentration"=>"089",
+         "sample_id"=>"8", "lane_number"=>"2"}
+      ]
     end
   end
 end
