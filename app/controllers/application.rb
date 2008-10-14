8dbac1309f710699e2af819ac9857fa43cd09a5c
bmarzolf <bmarzolf@53153c1e-2216-0410-80fa-f1be67669deb>
2008-10-14 22:08:32 +0000

    Rails 2.1.1 upgrade, plus turning on forgery protection and using cookie-based sessions.

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -12,4 +12,8 @@ class ApplicationController < ActionController::Base
   
   # filter passwords out of logs
   filter_parameter_logging "password"
+  
+  # See ActionController::RequestForgeryProtection for details
+  # Uncomment the :secret if you're not using the cookie session store
+  protect_from_forgery # :secret => 'c1e0cc867735b750b889ccc506dd107f'
 end
--- config/environment.rb
+++ config/environment.rb
@@ -5,7 +5,7 @@
 # ENV['RAILS_ENV'] ||= 'production'
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.0.2' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.1.1' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -45,9 +45,6 @@ Rails::Initializer.run do |config|
   # which shouldn't be used to store highly confidential information
   # (create the session table with 'rake db:sessions:create')
   # config.action_controller.session_store = :active_record_store
-  
-  # Use pstore, since we're storing > 4K in session
-  config.action_controller.session_store = :p_store
   
   # Use SQL instead of Active Record's schema dumper when creating the test database.
   # This is necessary if your schema can't be completely dumped by the schema dumper,
