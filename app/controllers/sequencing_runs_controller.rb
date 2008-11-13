3ea5e2aff69ce5a7a4e3f79e0d47ea70f451d991
bmarzolf <bmarzolf@53153c1e-2216-0410-80fa-f1be67669deb>
2008-11-13 16:54:15 +0000

    Fix sequencing_runs/index so that it shows runs in descending order.

--- app/controllers/sequencing_runs_controller.rb
+++ app/controllers/sequencing_runs_controller.rb
@@ -5,7 +5,7 @@ class SequencingRunsController < ApplicationController
   # GET /sequencing_runs
   # GET /sequencing_runs.xml
   def index
-    @sequencing_runs = SequencingRun.find(:all, "date DESC")
+    @sequencing_runs = SequencingRun.find(:all, :order => "date DESC")
 
     respond_to do |format|
       format.html # index.html.erb
