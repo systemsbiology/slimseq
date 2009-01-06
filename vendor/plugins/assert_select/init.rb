a722358efcb5c15363bdb35a3914ed47a93c6891
bmarzolf <bmarzolf@53153c1e-2216-0410-80fa-f1be67669deb>
2009-01-06 18:08:17 +0000

    Remove assert_select plugin since it's not used with slimseq and causes a Rails 2.2.2 initialization error.

--- vendor/plugins/assert_select/init.rb
+++
@@ -1,3 +0,0 @@
-require File.join(File.dirname(__FILE__), "lib", "assert_select")
-require File.join(File.dirname(__FILE__), "lib", "html_selector")
-Test::Unit::Assertions.send :include, Test::Unit::AssertSelect
