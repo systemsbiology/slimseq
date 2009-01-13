cb60c53c721c3729481dbaf9abd39f4cb06d2aae
bmarzolf <bmarzolf@53153c1e-2216-0410-80fa-f1be67669deb>
2009-01-13 21:05:58 +0000

    Expose instruments in API.

--- app/models/instrument.rb
+++ app/models/instrument.rb
@@ -4,4 +4,14 @@ class Instrument < ActiveRecord::Base
   def name_with_version
     return "#{name} (#{instrument_version})"
   end
+
+  def detail_hash
+    return {
+      :id => id,
+      :name => name,
+      :serial_number => serial_number,
+      :instrument_version => instrument_version,
+      :updated_at => updated_at
+    }
+  end
 end
