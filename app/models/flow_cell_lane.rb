57eb5a4d22121b7fb8f04f0a648ede30e0df8d26
bmarzolf <bmarzolf@53153c1e-2216-0410-80fa-f1be67669deb>
2009-03-19 19:45:08 +0000

    Reflect movement of file_folder from LabGroup to LabGroupProfile

--- app/models/flow_cell_lane.rb
+++ app/models/flow_cell_lane.rb
@@ -115,7 +115,8 @@ class FlowCellLane < ActiveRecord::Base
   end
   
   def default_result_path
-    "#{SiteConfig.raw_data_root_path}/#{samples[0].project.lab_group.file_folder}/" +
+    lab_group_profile = LabGroupProfile.find_by_lab_group_id(samples[0].project.lab_group_id)
+    "#{SiteConfig.raw_data_root_path}/#{lab_group_profile.file_folder}/" +
     "#{samples[0].project.file_folder}/#{flow_cell.sequencing_runs.best[0].date_yymmdd}_" +
     "#{flow_cell.sequencing_runs.best[0].instrument.serial_number}_#{flow_cell.name}"
   end
