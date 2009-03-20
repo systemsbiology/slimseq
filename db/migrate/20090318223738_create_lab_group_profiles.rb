01144767d3ef16754841751f8e3551a1316418b0
bmarzolf <bmarzolf@53153c1e-2216-0410-80fa-f1be67669deb>
2009-03-20 20:04:32 +0000

    Set default file_folder to ""

--- db/migrate/20090318223738_create_lab_group_profiles.rb
+++ db/migrate/20090318223738_create_lab_group_profiles.rb
@@ -2,7 +2,7 @@ class CreateLabGroupProfiles < ActiveRecord::Migration
   def self.up
     create_table :lab_group_profiles do |t|
       t.integer :lab_group_id
-      t.string :file_folder
+      t.string :file_folder, :default => ""
 
       t.timestamps
     end
