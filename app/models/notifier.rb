811ba3de906e9c0b62fbfe0f05dc0ff79e5159ec
bmarzolf <bmarzolf@53153c1e-2216-0410-80fa-f1be67669deb>
2009-03-24 19:03:40 +0000

    Use UserProfile instead of User in Notifier.

--- app/models/notifier.rb
+++ app/models/notifier.rb
@@ -1,14 +1,14 @@
 class Notifier < ActionMailer::Base
 
   def sample_submission_notification(samples)
-    recipients User.notify_of_new_samples.collect{|x| x.email}.join(",")
+    recipients UserProfile.notify_of_new_samples.collect{|x| x.user.email}.join(",")
     from       %("SLIMseq" <slimseq@#{`hostname`.strip}>)
     subject    "[SLIMseq] Samples recorded"
     body       :samples => samples
   end
 
   def sequencing_run_notification(sequencing_run)
-    recipients User.notify_of_new_sequencing_runs.collect{|x| x.email}.join(",")
+    recipients UserProfile.notify_of_new_sequencing_runs.collect{|x| x.user.email}.join(",")
     from       %("SLIMseq" <slimseq@#{`hostname`.strip}>)
     subject    "[SLIMseq] New sequencing run created"
     body       :sequencing_run => sequencing_run
