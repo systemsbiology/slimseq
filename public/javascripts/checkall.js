0ee91e3be329b2dff390367377551f42a64144c4
Bruz Marzolf <bmarzolf@systemsbiology.org>
2009-04-03 12:45:33 -0700

    Added support for a "master checker" to toggle checkboxes.

--- public/javascripts/checkall.js
+++ public/javascripts/checkall.js
@@ -1,11 +1,17 @@
 function checkAll(field)
 {
 for (i = 0; i < field.length; i++)
-	field[i].checked = true ;
+  field[i].checked = true ;
 }
 
 function uncheckAll(field)
 {
-	for (i = 0; i < field.length; i++)
-		field[i].checked = false ;
+  for (i = 0; i < field.length; i++)
+    field[i].checked = false ;
+}
+function toggleChecks(field)
+{
+  check_choice = $('master_checker').checked
+  for (i = 0; i < field.length; i++)
+    field[i].checked = check_choice;
 }
