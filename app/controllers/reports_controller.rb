9a2563394236f5442bcde8b64997394b4ddb4a03
Bruz Marzolf <bmarzolf@systemsbiology.org>
2009-04-03 15:54:04 -0700

    Get rid of old pagination call for billing report.

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -3,11 +3,11 @@ class ReportsController < ApplicationController
   before_filter :staff_or_admin_required
 
   def billing
-    @paged_lanes = FlowCellLane.paginate :page => params[:page],
+    @paged_lanes = FlowCellLane.find(:all,
         :include => [:samples => [:sample_prep_kit] , :flow_cell => :sequencing_runs ],
         :order => 'sequencing_runs.date DESC, samples.budget_number ASC',
         #:conditions => {:status => 'completed', :samples => {:control => 'false'} }
-        :conditions => "samples.status = 'completed' AND samples.control = 'false'"
+        :conditions => "samples.status = 'completed' AND samples.control = 'false'")
     @users_by_id = User.all_by_id
   end
 
