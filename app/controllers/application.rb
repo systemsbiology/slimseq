d7c2df79c952b0fe6354b9fbe1b32534f7f43792
Bruz Marzolf <bmarzolf@systemsbiology.org>
2009-04-08 09:45:39 -0700

    Turn off cross-site forgery protection so that POST /pipeline_runs web service will work. This isn't a big deal since we're running slimseq behind a firewall.

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -15,5 +15,5 @@ class ApplicationController < ActionController::Base
   
   # See ActionController::RequestForgeryProtection for details
   # Uncomment the :secret if you're not using the cookie session store
-  protect_from_forgery # :secret => 'c1e0cc867735b750b889ccc506dd107f'
+  # protect_from_forgery # :secret => 'c1e0cc867735b750b889ccc506dd107f'
 end
