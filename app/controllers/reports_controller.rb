eb01b36dcf651a586d627ee92c964a420ef8c531
Bruz Marzolf <bmarzolf@systemsbiology.org>
2009-08-25 12:19:29 -0700

    Include sequenced samples in billing report in addition to just the completed ones

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -6,8 +6,8 @@ class ReportsController < ApplicationController
     @paged_lanes = FlowCellLane.find(:all,
         :include => [:samples => [:sample_prep_kit] , :flow_cell => :sequencing_runs ],
         :order => 'sequencing_runs.date DESC, samples.budget_number ASC',
-        #:conditions => {:status => 'completed', :samples => {:control => 'false'} }
-        :conditions => "samples.status = 'completed' AND samples.control = 'false'")
+        :conditions => "(samples.status = 'completed' OR samples.status = 'sequenced')" +
+          " AND samples.control = 'false'")
     @users_by_id = User.all_by_id
   end
 
