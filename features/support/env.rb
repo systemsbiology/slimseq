c5bc913323ed6a35590f9ae0f95e60b1f3786cb0
Bruz Marzolf <bmarzolf@systemsbiology.org>
2009-11-12 13:57:08 -0800

    Updated version of cucumber.

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,13 +1,13 @@
 # Sets up the Rails environment for Cucumber
-ENV["RAILS_ENV"] = "test"
+ENV["RAILS_ENV"] ||= "test"
 require File.expand_path(File.dirname(__FILE__) + '/../../config/environment')
 require 'cucumber/rails/world'
-require 'cucumber/formatters/unicode'
+require 'cucumber/formatter/unicode' # Comment out this line if you don't want Cucumber Unicode support
+Cucumber::Rails.bypass_rescue # Comment out this line if you want Rails own error handling 
+                              # (e.g. rescue_action_in_public / rescue_responses / rescue_from)
+
 require 'webrat/rails'
 require 'cucumber/rails/rspec'
+require 'webrat/core/matchers'
 
-# Load site-wide config via fixtures
-Fixtures.create_fixtures("spec/fixtures", "site_config")
-
-# use FixtureReplacement plugin
 include FixtureReplacement
--- features/support/plain.rb
+++ features/support/plain.rb
@@ -1,1 +1,6 @@
 Cucumber::Rails.use_transactional_fixtures
+
+Webrat.configure do |config|
+  config.mode = :rails
+end
+
