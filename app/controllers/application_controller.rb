b8d82fc7c78e869fa78a91ea68d8e63770027f51
Bruz Marzolf <bmarzolf@systemsbiology.org>
2010-02-09 09:16:32 -0800

    Start using proper HTTP status codes and content-type-specific responses for common exceptions (in production).

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,13 @@ class ApplicationController < ActionController::Base
   # Homebrew, very simple authorization
   include Authorization
 
+  # Use proper HTTP status code and response format for common exceptions
+  unless ActionController::Base.consider_all_requests_local
+    rescue_from 'ActionController::UnknownAction', :with => :unknown_action
+    rescue_from 'ActionController::MethodNotAllowed', :with => :invalid_method
+    rescue_from 'ActiveSupport::JSON::ParseError', :with => :bad_json
+  end
+
   # Exception Notifier plugin
   include ExceptionNotifiable
   
@@ -16,9 +23,26 @@ class ApplicationController < ActionController::Base
   # See ActionController::RequestForgeryProtection for details
   # Uncomment the :secret if you're not using the cookie session store
   # protect_from_forgery # :secret => 'c1e0cc867735b750b889ccc506dd107f'
-
-  rescue_from 'ActiveSupport::JSON::ParseError' do |exception|
-    render :text => exception.to_s, :status => 422
-  end
   
+  protected
+
+    def invalid_method(exception)
+      error_with_status(exception, :method_not_allowed, 500)
+    end
+
+    def bad_json(exception)
+      error_with_status(exception, :unprocessable_entity, 500)
+    end
+
+    def unknown_action(exception)
+      error_with_status(exception, :not_found, 404)
+    end
+
+    def error_with_status(exception, status, html_error_file)
+      respond_to do |format|
+        format.html { render_optional_error_file(html_error_file) }
+        format.xml  { render :xml => {:error => exception.to_s}, :status => status }
+        format.json  { render :json => {:error => exception.to_s}, :status => status }
+      end
+    end
 end
