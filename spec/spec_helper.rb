466f57a767b1b9efe932f3df50620959297a975f
Bruz Marzolf <bmarzolf@systemsbiology.org>
2010-02-10 13:27:14 -0800

    Upgrade to rspec 1.3.0, rspec-rails 1.3.2

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,14 +2,17 @@ require 'rubygems'
 require 'spork'
 
 Spork.prefork do
-  # Loading more in this block will cause your tests to run faster. However, 
-  # if you change any configuration or code from libraries loaded here, you'll
-  # need to restart spork for it take effect.
-  
-  ENV["RAILS_ENV"] = "test"
-  require File.expand_path(File.dirname(__FILE__) + "/../config/environment")
-  require 'spec'
+  ENV["RAILS_ENV"] ||= 'test'
+  require File.expand_path(File.join(File.dirname(__FILE__),'..','config','environment'))
+  require 'spec/autorun'
   require 'spec/rails'
+
+  # Uncomment the next line to use webrat's matchers
+  #require 'webrat/integrations/rspec-rails'
+
+  # Requires supporting files with custom matchers and macros, etc,
+  # in ./support/ and its subdirectories.
+  Dir[File.expand_path(File.join(File.dirname(__FILE__),'support','**','*.rb'))].each {|f| require f}
 
   Spec::Runner.configure do |config|
     # If you're not using ActiveRecord you should remove these
