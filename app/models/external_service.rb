ba921fb2e22876676eff7560f58daeff05a7c5ab
Bruz Marzolf <bmarzolf@systemsbiology.org>
2010-02-10 15:05:38 -0800

    POST to external services with JSON

--- app/models/external_service.rb
+++ app/models/external_service.rb
@@ -3,12 +3,16 @@ class ExternalService < ActiveRecord::Base
   def self.sample_status_change(sample)
     # notify any services that want notifications on all samples
     find(:all, :conditions => {:sample_status_notification => true}).each do |service|
-      RestClient.post service.uri, :sample => {:id => sample.id, :status => sample.status}
+      RestClient.post service.uri,
+        { :sample => {:id => sample.id, :status => sample.status} }.to_json,
+        :content_type => :json, :accept => :json
     end
 
     # notify via the postback URI for the sample if it exists
     if postback_uri = sample.postback_uri
-      RestClient.post postback_uri, :sample => {:id => sample.id, :status => sample.status}
+      RestClient.post postback_uri,
+        { :sample => {:id => sample.id, :status => sample.status} }.to_json,
+        :content_type => :json, :accept => :json
     end
   end
 
