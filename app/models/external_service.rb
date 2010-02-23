c4746a80292e452bd29af5748cf15b6fbde08242
Bruz Marzolf <bmarzolf@systemsbiology.org>
2010-02-23 11:05:45 -0800

    Catch and log exceptions when trying to submit sample update notifications to external services.

--- app/models/external_service.rb
+++ app/models/external_service.rb
@@ -1,18 +1,22 @@
 class ExternalService < ActiveRecord::Base
 
   def self.sample_status_change(sample)
-    # notify any services that want notifications on all samples
-    find(:all, :conditions => {:sample_status_notification => true}).each do |service|
-      RestClient.post service.uri,
-        { :sample => {:id => sample.id, :status => sample.status} }.to_json,
-        :content_type => :json, :accept => :json
-    end
+    begin
+      # notify any services that want notifications on all samples
+      find(:all, :conditions => {:sample_status_notification => true}).each do |service|
+        RestClient.post service.uri,
+          { :sample => {:id => sample.id, :status => sample.status} }.to_json,
+          :content_type => :json, :accept => :json
+      end
 
-    # notify via the postback URI for the sample if it exists
-    if postback_uri = sample.postback_uri
-      RestClient.post postback_uri,
-        { :sample => {:id => sample.id, :status => sample.status} }.to_json,
-        :content_type => :json, :accept => :json
+      # notify via the postback URI for the sample if it exists
+      if postback_uri = sample.postback_uri
+        RestClient.post postback_uri,
+          { :sample => {:id => sample.id, :status => sample.status} }.to_json,
+          :content_type => :json, :accept => :json
+      end
+    rescue Errno::ECONNREFUSED => e
+      logger.error "Connection refused for external service notification on sample id #{sample.id}"
     end
   end
 
--- spec/models/external_service_spec.rb
+++ spec/models/external_service_spec.rb
@@ -1,19 +1,30 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe ExternalService do
+  describe "for a sample status change notification" do
+    it "should notify appropriate external services" do
+      service_1 = create_external_service(:uri => "http://localhost:4567/done")
+      service_2 = create_external_service(:sample_status_notification => false)
+      sample = mock_model(Sample, :status => "completed", :postback_uri => "http://localhost:4568/samples/1")
 
-  it "should notify appropriate external services of a sample status change notification" do
-    service_1 = create_external_service(:uri => "http://localhost:4567/done")
-    service_2 = create_external_service(:sample_status_notification => false)
-    sample = mock_model(Sample, :status => "completed", :postback_uri => "http://localhost:4568/samples/1")
+      RestClient.should_receive(:post).with("http://localhost:4567/done",
+        "{\"sample\":{\"status\":\"completed\",\"id\":#{sample.id}}}", {:content_type=>:json, :accept=>:json})
 
-    RestClient.should_receive(:post).with("http://localhost:4567/done",
-      :sample => {:id => sample.id, :status => "completed"})
+      RestClient.should_receive(:post).with("http://localhost:4568/samples/1",
+        "{\"sample\":{\"status\":\"completed\",\"id\":#{sample.id}}}", {:content_type=>:json, :accept=>:json})
 
-    RestClient.should_receive(:post).with("http://localhost:4568/samples/1",
-      :sample => {:id => sample.id, :status => "completed"})
+      ExternalService.sample_status_change(sample)
+    end
 
-    ExternalService.sample_status_change(sample)
+    it "should log an error if connecting to an external service fails" do
+      service_1 = create_external_service(:uri => "http://localhost:4567/done")
+      sample = mock_model(Sample, :status => "completed")
+      
+      RestClient.should_receive(:post).with("http://localhost:4567/done",
+        "{\"sample\":{\"status\":\"completed\",\"id\":#{sample.id}}}", {:content_type=>:json, :accept=>:json}).
+        and_raise(Errno::ECONNREFUSED)
+
+      lambda {ExternalService.sample_status_change(sample)}.should_not raise_error
+    end
   end
-
 end
