91d05788516caebf2b50a2a0cdc386e05a95a6c2
Bruz Marzolf <bmarzolf@systemsbiology.org>
2010-04-15 10:55:51 -0700

    Update ExternalService model for multiplexing

--- app/models/external_service.rb
+++ app/models/external_service.rb
@@ -5,14 +5,14 @@ class ExternalService < ActiveRecord::Base
       # notify any services that want notifications on all samples
       find(:all, :conditions => {:sample_status_notification => true}).each do |service|
         RestClient.post service.uri,
-          { :sample => {:id => sample.id, :status => sample.status} }.to_json,
+          { :sample => {:id => sample.id, :status => sample.sample_mixture.status} }.to_json,
           :content_type => :json, :accept => :json
       end
 
       # notify via the postback URI for the sample if it exists
       if postback_uri = sample.postback_uri
         RestClient.post postback_uri,
-          { :sample => {:id => sample.id, :status => sample.status} }.to_json,
+          { :sample => {:id => sample.id, :status => sample.sample_mixture.status} }.to_json,
           :content_type => :json, :accept => :json
       end
     rescue Errno::ECONNREFUSED => e
--- spec/models/external_service_spec.rb
+++ spec/models/external_service_spec.rb
@@ -5,7 +5,9 @@ describe ExternalService do
     it "should notify appropriate external services" do
       service_1 = create_external_service(:uri => "http://localhost:4567/done")
       service_2 = create_external_service(:sample_status_notification => false)
-      sample = mock_model(Sample, :status => "completed", :postback_uri => "http://localhost:4568/samples/1")
+      sample_mixture = mock_model(SampleMixture, :status => "completed")
+      sample = mock_model(Sample, :postback_uri => "http://localhost:4568/samples/1",
+        :sample_mixture => sample_mixture)
 
       RestClient.should_receive(:post).with("http://localhost:4567/done",
         "{\"sample\":{\"status\":\"completed\",\"id\":#{sample.id}}}", {:content_type=>:json, :accept=>:json})
@@ -18,7 +20,9 @@ describe ExternalService do
 
     it "should log an error if connecting to an external service fails" do
       service_1 = create_external_service(:uri => "http://localhost:4567/done")
-      sample = mock_model(Sample, :status => "completed")
+      sample_mixture = mock_model(SampleMixture, :status => "completed")
+      sample = mock_model(Sample, :postback_uri => "http://localhost:4568/samples/1",
+        :sample_mixture => sample_mixture)
       
       RestClient.should_receive(:post).with("http://localhost:4567/done",
         "{\"sample\":{\"status\":\"completed\",\"id\":#{sample.id}}}", {:content_type=>:json, :accept=>:json}).
