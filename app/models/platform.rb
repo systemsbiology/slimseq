9bdc3a0bbe2d16ab7f92c6060d475954a6a9c910
Bruz Marzolf <bmarzolf@systemsbiology.org>
2010-11-08 14:54:20 -0800

    Better staff home page links for different platforms

--- app/models/platform.rb
+++ app/models/platform.rb
@@ -3,6 +3,9 @@ class Platform < ActiveRecord::Base
   has_many :sample_prep_kits
   has_many :sample_mixtures
 
+  named_scope :flow_cell_and_sequencing_separate, :conditions => {:flow_cell_and_sequencing_separate => true}
+  named_scope :flow_cell_and_sequencing_combined, :conditions => {:flow_cell_and_sequencing_separate => false}
+
   def lane_number_options
     samples_per_flow_cell.split(/\s*,\s*/).collect{|n| n.to_i}
   end
