a453bbd614376940153f8afd7c9613c3993c05d8
Bruz Marzolf <bmarzolf@systemsbiology.org>
2010-12-01 12:33:31 -0800

    Remove code replaced by accepts_nested_attributes_for

--- app/models/flow_cell.rb
+++ app/models/flow_cell.rb
@@ -26,22 +26,6 @@ class FlowCell < ActiveRecord::Base
     transitions :from => :sequenced, :to => :completed    
   end
 
-  #def new_lane_attributes=(lane_attributes)
-  #  lane_attributes.each do |attributes|
-  #    flow_cell_lanes.build(attributes)
-  #  end
-  #end
-  #
-  #def existing_lane_attributes=(lane_attributes)
-  #  flow_cell_lanes.reject(&:new_record?).each do |lane|
-  #    attributes = lane_attributes[lane.id.to_s]
-  #    if attributes
-  #      lane.attributes = attributes
-  #      lane.save
-  #    end
-  #  end
-  #end
-  
   def summary_hash
     return {
       :id => id,
